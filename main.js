61106d2ebf801718415a996fada839354668c2b8
OFsdr <Ofsdr@proton.me>
2026-01-27 17:02:36 -0500

    test of Popups

---
+++ main.js
@@ -0,0 +1,8 @@
+function showLog() {
+    document.getElementById("log").style.display = "block"
+    document.getElementById("overlay").style.display = 'block'
+}
+function hiddenLog() {
+    document.getElementById('log').style.display = "none"
+    document.getElementById("overlay").style.display = 'none'
+}
