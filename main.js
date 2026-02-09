ee1cfce0d391a56a42a0454305687eb35069a2a6
OFsdr <Ofsdr@proton.me>
2026-02-08 21:40:28 -0500

    second test section about scroll

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function hiddenLog() {
 
 let index = 0;
 
+
 function next() {
   index++;
   document.getElementById("slides").style.transform =
@@ -18,5 +19,19 @@ function next() {
 function prev() {
   index--;
   document.getElementById("slides").style.transform =
+    `translateX(-${index * 100}%)`;
+}
+
+let index1 = 0;
+
+function next1() {
+  index1++;
+  document.getElementById("slides1").style.transform =
+    `translateX(-${index * 100}%)`;
+}
+
+function prev1() {
+  index1--;
+  document.getElementById("slides1").style.transform =
     `translateX(-${index * 100}%)`;
 }
